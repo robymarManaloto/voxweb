89dbef913e5d4319d7131eb35bed28e87e478fc7
robymarManaloto <manaloto.robymarlouise@auf.edu.ph>
2023-11-17 23:00:38 +0800

    Preparing to Deploy

--- voxweb/settings.py
+++ voxweb/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-v52xfvduc!72y@_^uq*vddpf^4d_m607#5y-&23mfri7)+jc$q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
